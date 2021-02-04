b5b8704bd0868e117158b8447829c94c80c687c9
yatheesh <yatheesh.konduru@gmail.com>
2021-02-04 22:04:18 +0530

    Updates

---
+++ precache-manifest.5715ea73612bc975087938393826970b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f435dd3089475f21c947cd98224c4f1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "165063fa6fa570cf9202",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "6cf44ff431dc3bdd5946",
+    "url": "./static/js/2.6f0eecbd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6f0eecbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165063fa6fa570cf9202",
+    "url": "./static/js/main.92260343.chunk.js"
+  },
+  {
+    "revision": "281712fc2f1b5650b1bb",
+    "url": "./static/js/runtime-main.c7d9a956.js"
+  }
+]);
